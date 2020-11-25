dc80b1c29d03865ee52495b0fe1a4ddca6370d9e
Jackson Morgan <67020956+jpmorgan98@users.noreply.github.com>
2020-11-25 12:53:04 -0800

    Added new member

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,97 @@
+var store = [{
+        "title": "Aaron Jame Reynolds",
+        "excerpt":"Education     Ph.D. Nuclear Engineering, Oregon State University (2020)   B.S. Electrical Engineering, Montana State University (2015)   At Oregon State since 2020.   Aaron Reynolds’ research interests include:     methods development for reactor analysis   multiphysics coupling using multilevel projective approaches   His efforts at CEMeNT focus on development of a time-dependent hybrid deterministic/Monte Carlo method to be used on modern computing architectures.   Prior to working with CEMeNT, Reynolds worked with Oak Ridge National Laboratory developing control rod depletion and fuel performance modeling capabilities in the the Virtual Environment for Reactor Applications. In his dissertation research, a multilevel nonlinear projective method in cylindrical geometry was developed for circulating fuel reactor kinetics.   LinkedIn: https://www.linkedin.com/in/aaron-james-reynolds-23b0b9a1/   GitHub: https://github.com/aaronjamesreynolds   Email: reynolaa@oregonstate.edu  ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/members/aaron_james_reynolds/",
+        "teaser": "http://localhost:4000/assets/bio/aaron_james_reynolds.jpg"
+      },{
+        "title": "Braxton Cuneo",
+        "excerpt":"Education     B.S.   M.S.   Ph.D.   Bio   Research   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/members/braxton_cuneo/",
+        "teaser": "http://localhost:4000/assets/bio/braxton_cuneo.jpg"
+      },{
+        "title": "Camille Palmer",
+        "excerpt":"Education     B.S.   M.S.   Ph.D.   Bio   Research   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/members/camille_palmer/",
+        "teaser": "http://localhost:4000/assets/bio/camille_palmer.jpg"
+      },{
+        "title": "Charles Goodman",
+        "excerpt":"Education     B.S.   M.S.   Ph.D.   Bio   Research   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/members/charles_goodman/",
+        "teaser": "http://localhost:4000/assets/bio/charles_goodman.jpg"
+      },{
+        "title": "C.T. Kelly",
+        "excerpt":"Education     B.S.   M.S.   Ph.D.   Bio   Research   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/members/ct_kelly/",
+        "teaser": "http://localhost:4000/assets/bio/bio-photo.jpg"
+      },{
+        "title": "Dmitriy Anistratov",
+        "excerpt":"Education     B.S.   M.S.   Ph.D.   Bio   Research   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/members/dmitriy_anistratov/",
+        "teaser": "http://localhost:4000/assets/bio/dmitriy_anistratov.jpg"
+      },{
+        "title": "Jackson Morgan",
+        "excerpt":"Education     BS, Mechanical Engineering, Oregon Institute of Technology, 2020   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/members/jackson_morgan/",
+        "teaser": "http://localhost:4000/assets/bio/jackson_morgan.jpg"
+      },{
+        "title": "Kayla Clements",
+        "excerpt":"Education     BS, FSU   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/members/kayla_clements/",
+        "teaser": "http://localhost:4000/assets/bio/kayla_clements.jpg"
+      },{
+        "title": "Kyle Niemeyer",
+        "excerpt":"Education     Ph.D., Mechanical Engineering, Case Western Reserve University, 2013   MS, Aerospace Engineering, Case Western Reserve University, 2010   BS, Aerospace Engineering, Case Western Reserve University, 2009   Research Interests  Dr. Niemeyer’s research focuses on the development of advanced numerical methods for modeling of combustion and reactive flows. His recent efforts included developing chemical-reaction mechanism reduction tools and algorithms for graphics processing units (GPUs) in order to enable the use of detailed, accurate chemical models in combustion simulations. Other research interests include computational modeling of multi-physics flows for applications in aerospace, transportation, and energy systems.   Biography  Kyle E. Niemeyer is an Associate Professor at the Oregon State University in the School of Mechanical, Industrial, and Manufacturing Engineering. He leads the Niemeyer Research Group, which uses computational modeling to study various phenomena involving fluid flows, including combustion and chemical kinetics, and related topics like numerical methods and parallel computing. He is also interested in open access, open source, and open science in general, and has contributed in the area of standardizing research software citation. Kyle received his Ph.D. in Mechanical Engineering from Case Western Reserve University in 2013, working with Jackie Sung and the Combustion Diagnostics Laboratory (now at the University of Connecticut). He received BS and MS degrees in Aerospace Engineering from Case Western Reserve University in 2009 and 2010, respectively.  ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/members/kyle_niemeyer/",
+        "teaser": "http://localhost:4000/assets/bio/kyle.jpg"
+      },{
+        "title": "Lizhong Chen",
+        "excerpt":"Education     B.S.   M.S.   Ph.D.   Bio   Research   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/members/lizhong.chen/",
+        "teaser": "http://localhost:4000/assets/bio/bio-photo.jpg"
+      },{
+        "title": "Mike Bailey",
+        "excerpt":"Education     B.S.   M.S.   Ph.D.   Bio   Research   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/members/mike_bailey/",
+        "teaser": "http://localhost:4000/assets/bio/bio-photo.jpg"
+      },{
+        "title": "Ryan McClarren",
+        "excerpt":"Education     B.S.   M.S.   Ph.D.   Bio   Research   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/members/ryan_mcclarren/",
+        "teaser": "http://localhost:4000/assets/bio/ryan_mcclarren.jpg"
+      },{
+        "title": "Sam Pasmann",
+        "excerpt":"Education     B.S.   M.S.   Ph.D.   Bio   Research   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/members/sam_pasmann/",
+        "teaser": "http://localhost:4000/assets/bio/bio-photo.jpg"
+      },{
+        "title": "Todd Palmer",
+        "excerpt":"Associate Dean for Faculty Advancement (College of Engineering), Professor   Radiation Transport and Reactor Physics Research Group   Education     Ph.D. Nuclear Engineering and Scientific Computing, University of Michigan (1993)   M.S. Nuclear Engineering (1988)   B.S. Nuclear Engineering, Oregon State University (1987)   At Oregon State since 1995.   Todd Palmer’s fields of interest include: numerical techniques for radiation transport and diffusion, reactor physics, general numerical methods, Monte Carlo methods, radiation transport in stochastic mixtures, and computational fluid dynamics.   Prior to Oregon State, Palmer served as a physicist, defense sciences, Lawrence Livermore National Laboratory (1991-1994). While at Oregon State, Palmer has served as a consultant to Lawrence Livermore National Laboratory, Battelle Pacific Northwest National Laboratory, Los Alamos National Laboratory, and Siemens Nuclear Power Corporation.  ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/members/todd_palmer/",
+        "teaser": "http://localhost:4000/assets/bio/todd_palmer.jpg"
+      },{
+        "title": "Vincent Novellino",
+        "excerpt":"Education     B.S.   M.S.   Ph.D.   Bio   Research   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/members/vincent_novellino/",
+        "teaser": "http://localhost:4000/assets/bio/vincent_novellino.jpg"
+      },{
+        "title": "Welcome",
+        "excerpt":"Welcome to the CEMeNT website where we will be posting updates about the project over the next few years.   Our kickoff meeting was conducted on August 18th 2020.   ","categories": ["Update"],
+        "tags": ["Update","Welcome"],
+        "url": "http://localhost:4000/update/welcome/",
+        "teaser": null
+      }]

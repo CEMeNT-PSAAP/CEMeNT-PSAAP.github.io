0d0a0e6abed713bf40a3ddbdc5196f43cf724c8d
Jackson Morgan <67020956+jpmorgan98@users.noreply.github.com>
2021-01-06 13:51:22 -0800

    spelling fix

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -1,5 +1,5 @@
 var store = [{
-        "title": "Aaron Jame Reynolds",
+        "title": "Aaron James Reynolds",
         "excerpt":"Education     Ph.D. Nuclear Engineering, Oregon State University (2020)   B.S. Electrical Engineering, Montana State University (2015)   At Oregon State since 2020.   Aaron Reynolds’ research interests include:     methods development for reactor analysis   multiphysics coupling using multilevel projective approaches   His efforts at CEMeNT focus on development of a time-dependent hybrid deterministic/Monte Carlo method to be used on modern computing architectures.   Prior to working with CEMeNT, Reynolds worked with Oak Ridge National Laboratory developing control rod depletion and fuel performance modeling capabilities in the the Virtual Environment for Reactor Applications. In his dissertation research, a multilevel nonlinear projective method in cylindrical geometry was developed for circulating fuel reactor kinetics.   LinkedIn: https://www.linkedin.com/in/aaron-james-reynolds-23b0b9a1/   GitHub: https://github.com/aaronjamesreynolds   Email: reynolaa@oregonstate.edu  ","categories": [],
         "tags": [],
         "url": "http://localhost:4000/members/aaron_james_reynolds/",

54a0811a73aa213c8c65431e62b9d13322021358
Jackson Morgan <67020956+jpmorgan98@users.noreply.github.com>
2021-01-26 16:34:33 -0800

    Added better bios
    Goodmann, Novellio, Morgan

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -12,7 +12,7 @@ var store = [{
         "teaser": "http://localhost:4000/assets/bio/braxton_cuneo.jpg"
       },{
         "title": "Camille Palmer",
-        "excerpt":"Education     B.S. Radiation Health Physics   M.S. Health Physics   Ph.D. Nuclear and Radiological Engineering   Bio  Dr. Camille Palmer is an associate professor in the School of Nuclear Science and Engineering at Oregon State University. Her professional interests emphasize international nuclear security and nonproliferation, where she enjoys collaborations with OSU’s School of Public Policy and Robotics. Prior to academics, she was a staff member in both the Thermonuclear Applications and Foreign and Improvised Nuclear Design groups at Los Alamos National Lab. She has also supported the nuclear hardness and survivability of the Minuteman III ground and delivery systems as an engineer with Northrop Grumman Corporation.   Research  Dr. Palmer is motivated by problems that impact national security and the international nuclear landscape related to energy and defense. She serves as faculty advisor to the OSU Chapter of the Institute for Nuclear Materials Management (INMM) and on the subcommittee for Arms Control and Treaty Verification within the Nonproliferation and Arms Control Technical Division of INMM.  She also a member of the editorial board for The Journal of Critical Infrastructure Policy.  ","categories": [],
+        "excerpt":"Education     B.S.   M.S.   Ph.D.   Bio   Research   ","categories": [],
         "tags": [],
         "url": "http://localhost:4000/members/camille_palmer/",
         "teaser": "http://localhost:4000/assets/bio/camille_palmer.jpg"
@@ -23,11 +23,11 @@ var store = [{
         "url": "http://localhost:4000/members/charles_goodman/",
         "teaser": "http://localhost:4000/assets/bio/charles_goodman.jpg"
       },{
-        "title": "C.T. Kelley",
-        "excerpt":"Education     B.S.Vanberbilt University, Mathematics, 1973   Ph.D. Purdue University, Applied Mathematics, 1976   Bio  At North Carolina State University since 1978.   Prior to arriving at NCSU, Kelley was an assistant scientist (aka postdoc) at the US Army Mathematics Research Center in Madison WI.   Kelley is a Drexel Professor of Mathematics and is a fellow of the Society for Industrial and Applied Mathematics (SIAM) and the American Assocaiation for the Advancement of Science (AAAS).   Research  Kelley’s research interests are in linear/nonlinear equations, radiative transfer problems, multilevel methods for integral equations, optimal control, optimization of noisy functions, flow in porous media, and computational quantum chemistry and physics.  ","categories": [],
+        "title": "C.T. Kelly",
+        "excerpt":"Education     B.S.   M.S.   Ph.D.   Bio   Research   ","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/members/ct_kelley/",
-        "teaser": "http://localhost:4000/assets/bio/ct_kelley.jpg"
+        "url": "http://localhost:4000/members/ct_kelly/",
+        "teaser": "http://localhost:4000/assets/bio/bio-photo.jpg"
       },{
         "title": "Dmitriy Anistratov",
         "excerpt":"Education     B.S.   M.S.   Ph.D.   Bio   Research   ","categories": [],
@@ -35,14 +35,8 @@ var store = [{
         "url": "http://localhost:4000/members/dmitriy_anistratov/",
         "teaser": "http://localhost:4000/assets/bio/dmitriy_anistratov.jpg"
       },{
-        "title": "Ilham Variansyah",
-        "excerpt":"Education     B.S.E., Nuclear Engineering, Universitas Gadjah Mada, 2014   M.S.E., Nuclear Engineering and Radiological Sciences, University of Michigan, 2017   Ph.D., Nuclear Engineering and Radiological Sciences, University of Michigan, 2020  ","categories": [],
-        "tags": [],
-        "url": "http://localhost:4000/members/ilham_variansyah/",
-        "teaser": "http://localhost:4000/assets/bio/ilham_variansyah.jpg"
-      },{
         "title": "Jackson Morgan",
-        "excerpt":"Education     BS, Mechanical Engineering, Oregon Institute of Technology, 2020   PhD, Mechanical Engineering, Oregon State University, 2025   ","categories": [],
+        "excerpt":"Education     BS, Mechanical Engineering, Oregon Institute of Technology, 2020   ","categories": [],
         "tags": [],
         "url": "http://localhost:4000/members/jackson_morgan/",
         "teaser": "http://localhost:4000/assets/bio/jackson_morgan.jpg"
@@ -60,31 +54,31 @@ var store = [{
         "teaser": "http://localhost:4000/assets/bio/kyle.jpg"
       },{
         "title": "Lizhong Chen",
-        "excerpt":"Education     B.S. Electrical Engineering, Zhejiang University, 2009   M.S. Electrical Engineering, University of Southern California, 2011   Ph.D. Computer Engineering, University of Southern California, 2014   Bio  Dr. Lizhong Chen is an Associate Professor in the School of Electrical Engineering and Computer Science at Oregon State University. His research interests are in the broad area of computer architecture, machine learning accelerators, GPUs, interconnection networks, and emerging IoT technologies. Dr. Chen is a recipient of National Science Foundation (NSF) CRII Award (2016), NSF CAREER Award (2018), Best Paper Nomination at IEEE NAS (2018), Best Paper Runner-up Award at HPCA (2020), and Chu Kochen Award (the highest honor from Zhejiang University, China). He currently serves as an Associate Editor of the IEEE Transactions on Computers (TC). Dr. Chen also served as a program committee member in top computer architecture conferences (e.g., ISCA, HPCA, DAC, MICRO (ERC), ICS, IPDPS, ICCD, IISWC, ISPASS), reviewer for a number of IEEE and ACM journals (e.g., TC, TPDS, TVLSI, TCAD, CAL, TACO, TETC, TDSC, TCSI, TODAES), and panelist of multiple NSF panels related to computer systems architecture. He is the founder and organizer of the Annual International Workshop on AIDArc (AI-assisted Design for Architecture), held in conjunction with ISCA. Dr. Chen is a Senior Member of the IEEE and ACM, and an inductee of the HPCA Hall of Frame.   Prior to join Oregon State University in 2014, Dr. Chen was a Postdoctoral Research Associate at the University of Southern California (USC). He received his Ph.D. in Computer Engineering and M.S. in Electrical Engineering from USC (advisor Dr. Timothy Pinkston) in 2014 and 2011, respectively, and B.S. in Electrical Engineering from Zhejiang University in 2009.   ","categories": [],
+        "excerpt":"Education     B.S.   M.S.   Ph.D.   Bio   Research   ","categories": [],
         "tags": [],
-        "url": "http://localhost:4000/members/lizhong_chen/",
-        "teaser": "http://localhost:4000/assets/bio/lizhong_chen.jpg"
+        "url": "http://localhost:4000/members/lizhong.chen/",
+        "teaser": "http://localhost:4000/assets/bio/bio-photo.jpg"
       },{
         "title": "Mike Bailey",
-        "excerpt":"Education  B.S. Mechanical Engineering, Purdue University, 1975 M.S. Computer Graphics and Computer Aided Design, Purdue University, 1976 Ph.D. Computer Aided Design and Computer Graphics, Purdue University, 1979   Bio  Mike Bailey is a professor in computer science at Oregon State University. His areas of interest include scientific visualization, high performance computer graphics, solid freeform fabrication, geometric modeling, and computer aided design and analysis.   Mike received his Ph.D. from Purdue University in computer graphics and computer aided design in 1979. From 1979-1981 he was a member of Sandia National Laboratories’ technical staff, specializing in developing 3D graphics tools for mechanical designers. From 1981-1985, Mike was an assistant and associate professor at Purdue University. In 1985, Mike became the director of advanced development at Megatek Corporation, where he managed a group of engineers who were charged with developing Megatek’s next generation of computer graphics technology. From 1989-2004 Mike was the director of visualization at the San Diego Supercomputer Center and an adjunct professor at the University of California San Diego. He joined the Oregon State faculty in 2004.   Mike is a member of the Association of Computing Machinery (ACM), the Special Interest Group on Computer Graphics (ACM-SIGGRAPH), the Institute of Electrical and Electronics Engineers (IEEE), and the American Society of Mechanical Engineers (ASME). He has served on a number of conference committees, including co-chairing the 1991 SIGGRAPH international conference and the IEEE Visualization conference in 2001.   Mike has presented numerous technical papers and short courses at a variety of conferences and workshops.   Mike has taught a variety of college classes to over 2,900 students. UCSD’s graduating computer science seniors voted him Teacher of the Year 5 years in a row, from 1997-2002.   ","categories": [],
+        "excerpt":"Education     B.S.   M.S.   Ph.D.   Bio   Research   ","categories": [],
         "tags": [],
         "url": "http://localhost:4000/members/mike_bailey/",
-        "teaser": "http://localhost:4000/assets/bio/mike_bailey.jpg"
+        "teaser": "http://localhost:4000/assets/bio/bio-photo.jpg"
       },{
         "title": "Ryan McClarren",
-        "excerpt":"Education     B.S.E., Nuclear Engineering and Radiological Sciences, University of Michigan, 2003   M.S.E., Nuclear Engineering and Radiological Sciences, University of Michigan, 2004   Ph.D., Nuclear Engineering and Radiological Sciences, University of Michigan, 2007  ","categories": [],
+        "excerpt":"Education     B.S.   M.S.   Ph.D.   Bio   Research   ","categories": [],
         "tags": [],
         "url": "http://localhost:4000/members/ryan_mcclarren/",
         "teaser": "http://localhost:4000/assets/bio/ryan_mcclarren.jpg"
       },{
         "title": "Sam Pasmann",
-        "excerpt":"Education     B.S. Mechanical Engineering, University of Portland   M.S. Engineering and Entrepreneurship, University of Notre Dame   ","categories": [],
+        "excerpt":"Education     B.S.   M.S.   Ph.D.   Bio   Research   ","categories": [],
         "tags": [],
         "url": "http://localhost:4000/members/sam_pasmann/",
-        "teaser": "http://localhost:4000/assets/bio/sam_pasmann.jpg"
+        "teaser": "http://localhost:4000/assets/bio/bio-photo.jpg"
       },{
         "title": "Todd Palmer",
-        "excerpt":"Professor   Lead PI, Radiation Transport and Reactor Physics Research Group   Education     Ph.D. Nuclear Engineering and Scientific Computing, University of Michigan   M.S. Nuclear Engineering   B.S. Nuclear Engineering, Oregon State University   At Oregon State since 1995.   Todd Palmer’s fields of interest include: numerical techniques for radiation transport and diffusion, reactor physics, general numerical methods, Monte Carlo methods, radiation transport in stochastic mixtures, and computational fluid dynamics.   Prior to Oregon State, Palmer served as a Physicist in Defense Sciences at Lawrence Livermore National Laboratory (1991-1994). While at Oregon State, Palmer has served as a consultant to Lawrence Livermore National Laboratory, Battelle Pacific Northwest National Laboratory, Los Alamos National Laboratory, and Siemens Nuclear Power Corporation.  ","categories": [],
+        "excerpt":"Associate Dean for Faculty Advancement (College of Engineering), Professor   Radiation Transport and Reactor Physics Research Group   Education     Ph.D. Nuclear Engineering and Scientific Computing, University of Michigan (1993)   M.S. Nuclear Engineering (1988)   B.S. Nuclear Engineering, Oregon State University (1987)   At Oregon State since 1995.   Todd Palmer’s fields of interest include: numerical techniques for radiation transport and diffusion, reactor physics, general numerical methods, Monte Carlo methods, radiation transport in stochastic mixtures, and computational fluid dynamics.   Prior to Oregon State, Palmer served as a physicist, defense sciences, Lawrence Livermore National Laboratory (1991-1994). While at Oregon State, Palmer has served as a consultant to Lawrence Livermore National Laboratory, Battelle Pacific Northwest National Laboratory, Los Alamos National Laboratory, and Siemens Nuclear Power Corporation.  ","categories": [],
         "tags": [],
         "url": "http://localhost:4000/members/todd_palmer/",
         "teaser": "http://localhost:4000/assets/bio/todd_palmer.jpg"
@@ -96,7 +90,7 @@ var store = [{
         "teaser": "http://localhost:4000/assets/bio/vincent_novellino.jpg"
       },{
         "title": "Welcome",
-        "excerpt":"Welcome to the CEMeNT website where we will be posting updates about the project over the next few years.   Our kickoff meeting was conducted on August 18th 2020 with our sildes avalible here.   ","categories": ["Update"],
+        "excerpt":"Welcome to the CEMeNT website where we will be posting updates about the project over the next few years.   Our kickoff meeting was conducted on August 18th 2020.   ","categories": ["Update"],
         "tags": ["Update","Welcome"],
         "url": "http://localhost:4000/update/welcome/",
         "teaser": null

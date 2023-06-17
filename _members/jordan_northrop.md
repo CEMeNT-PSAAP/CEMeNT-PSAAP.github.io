3107f200a3cd219d1471ffae6c0959e1b15578e7
jpmac (Joanna Piper Morgan) <joannapipermorgan@gmail.com>
2023-06-16 18:44:44 -0700

    updating jordan

--- _members/jordan_northrop.md
+++ _members/jordan_northrop.md
@@ -10,10 +10,9 @@ author: Jordan Northrop
 
 ---
 ## Education
-* B.S., Nuclear Engineering, Oregon State University (In progress)
+* B.S., Nuclear Engineering, Oregon State University (2022)
+* M.S., Nuclear Engineering, Oregon State University (In Progress)
 
 ## Bio
-Jordan Northrop is an undergraduate nuclear engineering student at Oregon State University. 
-Previous work includes an internship with the Westinghouse Fellows Program at Penn State using fission matrix methods to model TRIGA reactor pulses.
 
-Currently working with Dr. Camille Palmer and Dr. Todd Palmer to use the pulsed sphere benchmark problems to validate Monte Carlo codes.
+Jordan Northrop is a nuclear engineering graduate student at Oregon State University working with Dr. Camille Palmer and Dr. Todd Palmer to model and simulate a time dependent small modular reactor challenge problem using Monte Carlo and hybrid methods. Previous work includes modeling the pulsed sphere benchmark problems for CEMeNT, modeling pulsed neutron die away experiments for LLNL, and using fission matrix methods to model TRIGA reactor pulses as an intern for the Westinghouse Fellows Program.
--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -60,7 +60,7 @@ var store = [{
         "teaser": "/assets/bio/joanna_morgan.jpg"
       },{
         "title": "Jordan Northrop",
-        "excerpt":"Education     B.S., Nuclear Engineering, Oregon State University (In progress)   Bio  Jordan Northrop is an undergraduate nuclear engineering student at Oregon State University.  Previous work includes an internship with the Westinghouse Fellows Program at Penn State using fission matrix methods to model TRIGA reactor pulses.   Currently working with Dr. Camille Palmer and Dr. Todd Palmer to use the pulsed sphere benchmark problems to validate Monte Carlo codes.  ","categories": [],
+        "excerpt":"Education     B.S., Nuclear Engineering, Oregon State University (2022)   M.S., Nuclear Engineering, Oregon State University (In Progress)   Bio   Jordan Northrop is a nuclear engineering graduate student at Oregon State University working with Dr. Camille Palmer and Dr. Todd Palmer to model and simulate a time dependent small modular reactor challenge problem using Monte Carlo and hybrid methods. Previous work includes modeling the pulsed sphere benchmark problems for CEMeNT, modeling pulsed neutron die away experiments for LLNL, and using fission matrix methods to model TRIGA reactor pulses as an intern for the Westinghouse Fellows Program.  ","categories": [],
         "tags": [],
         "url": "/members/jordan_northrop/",
         "teaser": "/assets/bio/jordan_northrop.jpg"

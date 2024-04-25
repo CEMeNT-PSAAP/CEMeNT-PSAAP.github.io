c6c8630015c12c94cb2609d70d45c8cb31b0c3f5
Joanna Piper Morgan (jonsey) <jpmorgan98@gmail.com>
2024-04-25 09:02:00 -0700

    wip

--- _members/david_leep.md
+++ _members/david_leep.md
@@ -8,6 +8,9 @@ author: David Leep
 
 ---
 
+## Education
+* B.S. Nuclear Engineering, Oregon State University (June 2024)
+
 ## Biography
 
 At CEMeNT, David is working on developing a translation function to convert OpenMC input files into MC/DC compatible base-line code for user configuration. This allows for a quick and easy conversion of OpenMC systems into MC/DC counterparts, useful for comparison between applications and learning MC/DC formatting. 
--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -36,7 +36,7 @@ var store = [{
         "teaser": "/assets/bio/ct_kelley.jpg"
       },{
         "title": "David Leep",
-        "excerpt":"Biography   At CEMeNT, David is working on developing a translation function to convert OpenMC input files into MC/DC compatible base-line code for user configuration. This allows for a quick and easy conversion of OpenMC systems into MC/DC counterparts, useful for comparison between applications and learning MC/DC formatting.  ","categories": [],
+        "excerpt":"Education     B.S. Nuclear Engineering, Oregon State University (June 2024)   Biography   At CEMeNT, David is working on developing a translation function to convert OpenMC input files into MC/DC compatible base-line code for user configuration. This allows for a quick and easy conversion of OpenMC systems into MC/DC counterparts, useful for comparison between applications and learning MC/DC formatting.  ","categories": [],
         "tags": [],
         "url": "/members/david_leep/",
         "teaser": "/assets/bio/bio-photo.jpg"
